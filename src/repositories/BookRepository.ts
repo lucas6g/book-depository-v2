9782926339b0310cc597bb2658e2b2334552a457
lucas6g <lucas.trabalho1533@gmail.com>
2022-05-21 18:24:50 -0300

    feat: create user repository prisma and change return of method

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -1,7 +1,7 @@
 import { Book } from '../dtos/Book'
 
 export interface BookRepository {
-  save(userId: string, institutionId: string, book: Book.Input): Promise<void>;
+  save(userId: string, book: Book.Input): Promise<void>;
   getByName(name: string): Promise<Book.Output>;
   getByUser(userId: string): Promise<Book.Output[]>;
   getAll(): Promise<Book.Output[]>;
---
+++ src/repositories/implementations/BookRepositoryPrisma.ts
@@ -0,0 +1,95 @@
+import { prismaClient } from '../../database/prismaClient'
+import { Book } from '../../dtos/Book'
+import { BookRepository } from '../BookRepository'
+
+export class BookRepositoryPrisma implements BookRepository {
+  async save (userId: string, book: Book.Input): Promise<void> {
+    const { address, image } = book
+    await prismaClient.book.create({
+      data: {
+        title: book.title,
+        author: book.author,
+        edition: book.edition,
+        year: book.year,
+        publishedDate: book.publishedDate,
+        quantity: book.quantity,
+        bookCondition: book.bookCondition,
+        user: { connect: { id: userId } },
+        institution: { connect: { id: book.institutionId } },
+        image: {
+          create: {
+            id: image?.id!,
+            url: image?.url!,
+            name: image?.name!,
+            type: image?.type!,
+            size: image?.size!
+          }
+        },
+        address: {
+          create: {
+            street: address.street,
+            number: address.number,
+            reference: address.reference,
+            zip: address.zip,
+            city: address.city,
+            state: address.state
+          }
+        }
+      }
+    })
+  }
+
+  async getByName (name: string): Promise<Book.Output> {
+    throw new Error('Method not implemented.')
+  }
+
+  async getByUser (userId: string): Promise<Book.Output[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async getAll (): Promise<Book.Output[]> {
+    const books = await prismaClient.book.findMany({
+      include: {
+        institution: {
+          include: {
+            user: true,
+            address: true
+          }
+        },
+        image: true,
+        address: true
+      }
+    })
+    return books.map((book) => {
+      const { institution, image, address } = book
+      return {
+        id: book.id,
+        title: book.title,
+        author: book.author,
+        edition: book.edition,
+        year: book.year,
+        publishedDate: book.publishedDate,
+        quantity: book.quantity,
+        available: book.available,
+        bookCondition: book.bookCondition,
+        imageUrl: image.url,
+        institution: {
+          id: institution.id,
+          name: institution.name
+        },
+        user: {
+          id: institution.user.id,
+          name: institution.user.name
+        },
+        address: {
+          street: address.street,
+          number: address.number,
+          reference: address.reference,
+          zip: address.zip,
+          city: address.city,
+          state: address.state
+        }
+      }
+    })
+  }
+}

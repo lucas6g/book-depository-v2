ee9a52b69d8eb9bfbaebed476a15e644f4b1bd24
lucas6g <lucas.trabalho1533@gmail.com>
2022-05-21 18:21:41 -0300

    feat: add cors and urlencoded express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 import { router } from './routes'
 
 const app: Express = express()
 
 app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.use(router)
 

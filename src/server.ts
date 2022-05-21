9bea565dcdf94de29b8ffd3035e0d42ae29ddbf3
lucas6g <lucas.trabalho1533@gmail.com>
2022-05-21 19:05:42 -0300

    ci: heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import { router } from './routes'
+import { env } from 'process'
 
 const app: Express = express()
 
@@ -12,5 +13,13 @@ app.use(cors())
 
 app.use(router)
 
-const port = process.env.PORT || 5000
+const port = env.PORT || 5001
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+console.log(`Database URL: ${env.DATABASE_URL}`)
+console.log(`Email host: ${env.MAIL_HOST}`)
+console.log(`Email port: ${env.MAIL_PORT}`)
+console.log(`Email user: ${env.MAIL_USER}`)
+console.log(`Email password: ${env.MAIL_PASSWORD}`)
+console.log(`Email from: ${env.MAIL_FROM}`)
+console.log(`Environment: ${env.NODE_ENV}`)
